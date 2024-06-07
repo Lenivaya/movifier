047795145873cd873846a86df04c0ef0afb6c79d
Lenivaya <danylo.osipchuk@proton.me>
2024-06-07 14:07:57 +0300

    feat: years page

--- frontend/src/app/movies/[...filters]/page.tsx
+++ frontend/src/app/movies/[...filters]/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { MoviesSearchCriteriaInput } from '@/lib'
+import { MoviesSearchCriteriaInput, isString } from '@/lib'
 import { MoviesPage } from '@/components/movifier/movies/MoviesPage/MoviesPage'
 import { D } from '@mobily/ts-belt'
+import { match } from 'ts-pattern'
 
 type MovieFilters = Omit<MoviesSearchCriteriaInput, 'search'>
 const expectedFilters = ['genre', 'decade', 'year', 'keyword']
@@ -16,8 +17,12 @@ export default function Page({ params }: { params: { filters: string[] } }) {
 function parseFilters(filters: string[]): MovieFilters {
   const result = filters.reduce((acc, curr, index, array) => {
     if (index % 2 === 0 && array[index + 1]) {
+      const value = array[index + 1]
+
       // @ts-ignore
-      acc[curr] = array[index + 1].replace(/%20/g, ' ')
+      acc[curr] = match(curr)
+        .with('decade', () => parseInt(value))
+        .otherwise(() => value.replace(/%20/g, ' '))
     }
     return acc
   }, {} as MovieFilters)
